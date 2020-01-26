932b1ce921facebdfee2d6a30a8061c4e906f392
Kaido Henrik Elias <kaidoelias@gmail.com>
2020-01-26 22:51:21 +0200

    First commit

---
+++ fixScores/FixScoresProgram.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using fixScores.models;
+using Newtonsoft.Json;
+using static System.IO.File;
+
+namespace fixScores
+{
+	class Program
+	{
+		private static readonly string BeatSaverPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\AppData\LocalLow\Hyperbolic Magnetism\Beat Saber\";
+		public static Dictionary<string, BeatSaverSong> BeatSaverSongs;
+
+		private static void Main(string[] args)
+		{
+			ImportBeatSaverSongsFromApi();
+			var convertedLeaderBoard = ConvertLeaderBoard();
+			MakeCopyOfExistingLeaderBoard();
+			SaveNewLeaderBoard(convertedLeaderBoard);
+		}
+
+		private static void SaveNewLeaderBoard(PartyLeaderBoards partyLeaderBoards)
+		{
+			WriteAllText(BeatSaverPath + "LocalLeaderboards.dat", JsonConvert.SerializeObject(partyLeaderBoards));
+		}
+
+		private static void MakeCopyOfExistingLeaderBoard()
+		{
+			Move(BeatSaverPath + "LocalLeaderboards.dat", BeatSaverPath + "LocalLeaderboards_" + GetTimestamp(DateTime.Now) + ".dat");
+		}
+
+		public static string GetTimestamp(DateTime value)
+		{
+			return value.ToString("yyyyMMddHHmmssffff");
+		}
+
+		private static PartyLeaderBoards ConvertLeaderBoard()
+		{
+			var dictionary = new Dictionary<string, LeaderboardsData>();
+			foreach (var data in GetLocalLeaderBoardData())
+			{
+				var leaderBoardId = data._leaderboardId;
+				AddToDictionary(dictionary, leaderBoardId.Contains(" ") ? GetNewId(leaderBoardId) : leaderBoardId, data);
+			}
+
+			return new PartyLeaderBoards(dictionary.Values.ToList());
+		}
+
+		private static string GetNewId(string leaderBoardId)
+		{
+			var split = leaderBoardId.Split(" ");
+			var key = split[0].Split("_")[^1];
+			var hash = GetHashFromBeatSaverSongList(key);
+			var newId = "custom_level_" + hash.ToUpper() + split[^1].Split(")")[^1];
+			return newId;
+		}
+
+		private static void AddToDictionary(IDictionary<string, LeaderboardsData> dictionary, string leaderBoardId, LeaderboardsData data)
+		{
+			if (dictionary.TryGetValue(leaderBoardId, out var existingData))
+			{
+				existingData._scores.AddRange(data._scores);
+			}
+			else
+			{
+				dictionary.Add(leaderBoardId, new LeaderboardsData(leaderBoardId, data._scores));
+			}
+		}
+
+
+		private static string GetHashFromBeatSaverSongList(string key)
+		{
+			return BeatSaverSongs.TryGetValue(key, out var beatSaverSong) ? beatSaverSong.hash : "DEFAULT_HASH";
+		}
+
+		public static List<LeaderboardsData> GetLocalLeaderBoardData()
+		{
+			using var reader = new StreamReader(BeatSaverPath + "LocalLeaderboards.dat");
+			var json = reader.ReadToEnd();
+			return JsonConvert.DeserializeObject<PartyLeaderBoards>(json)._leaderboardsData;
+		}
+
+		public static void ImportBeatSaverSongsFromApi()
+		{
+			BeatSaverSongs = new Dictionary<string, BeatSaverSong>();
+			var request = (HttpWebRequest) WebRequest.Create(@"https://beatsaver.com/api/download/dump/maps");
+			request.AutomaticDecompression = DecompressionMethods.GZip;
+			var response = (HttpWebResponse) request.GetResponse();
+			var stream = response.GetResponseStream();
+			string fullListOfSongs;
+			using var reader = new StreamReader(stream);
+			{
+				fullListOfSongs = reader.ReadToEnd();
+			}
+			foreach (var beatSaverSong in JsonConvert.DeserializeObject<List<BeatSaverSong>>(fullListOfSongs))
+			{
+				BeatSaverSongs.Add(beatSaverSong.key, beatSaverSong);
+			}
+
+		}
+	}
+}
---
+++ fixScores/models/models.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fixScores.models
+{
+	public class Score
+	{
+		public int _score { get; set; }
+		public string _playerName { get; set; }
+		public bool _fullCombo { get; set; }
+		public int _timestamp { get; set; }
+	}
+
+	public class PartyLeaderBoards
+	{
+		public PartyLeaderBoards(List<LeaderboardsData> leaderboardsData)
+		{
+			_leaderboardsData = leaderboardsData;
+		}
+
+		public List<LeaderboardsData> _leaderboardsData { get; set; }
+	}
+
+	public class LeaderboardsData
+	{
+		public LeaderboardsData(string leaderboardId, List<Score> scores)
+		{
+			_leaderboardId = leaderboardId;
+			_scores = scores;
+		}
+
+		public string _leaderboardId { get; set; }
+		public List<Score> _scores { get; set; }
+
+	}
+
+	public class Difficulties
+	{
+		public bool easy { get; set; }
+		public bool expert { get; set; }
+		public bool expertPlus { get; set; }
+		public bool hard { get; set; }
+		public bool normal { get; set; }
+	}
+
+	public class Easy
+	{
+		public string duration { get; set; }
+		public string length { get; set; }
+		public string njs { get; set; }
+		public string njsOffset { get; set; }
+		public string bombs { get; set; }
+		public string notes { get; set; }
+		public string obstacles { get; set; }
+	}
+
+	public class Expert
+	{
+		public string duration { get; set; }
+		public string length { get; set; }
+		public string njs { get; set; }
+		public string njsOffset { get; set; }
+		public string bombs { get; set; }
+		public string notes { get; set; }
+		public string obstacles { get; set; }
+	}
+
+	public class ExpertPlus
+	{
+		public string duration { get; set; }
+		public string length { get; set; }
+		public string njs { get; set; }
+		public string njsOffset { get; set; }
+		public string bombs { get; set; }
+		public string notes { get; set; }
+		public string obstacles { get; set; }
+	}
+
+	public class Hard
+	{
+		public string duration { get; set; }
+		public string length { get; set; }
+		public string njs { get; set; }
+		public string njsOffset { get; set; }
+		public string bombs { get; set; }
+		public string notes { get; set; }
+		public string obstacles { get; set; }
+	}
+
+	public class Normal
+	{
+		public string duration { get; set; }
+		public string length { get; set; }
+		public string njs { get; set; }
+		public string njsOffset { get; set; }
+		public string bombs { get; set; }
+		public string notes { get; set; }
+		public string obstacles { get; set; }
+	}
+
+	public class Difficulties2
+	{
+		public Easy easy { get; set; }
+		public Expert expert { get; set; }
+		public ExpertPlus expertPlus { get; set; }
+		public Hard hard { get; set; }
+		public Normal normal { get; set; }
+	}
+
+	public class Characteristic
+	{
+		public Difficulties2 difficulties { get; set; }
+		public string name { get; set; }
+	}
+
+	public class Metadata
+	{
+		public Difficulties difficulties { get; set; }
+		public int duration { get; set; }
+		public List<Characteristic> characteristics { get; set; }
+		public string levelAuthorName { get; set; }
+		public string songAuthorName { get; set; }
+		public string songName { get; set; }
+		public string songSubName { get; set; }
+		public string bpm { get; set; }
+	}
+
+	public class Stats
+	{
+		public int downloads { get; set; }
+		public int plays { get; set; }
+		public int downVotes { get; set; }
+		public int upVotes { get; set; }
+		public double heat { get; set; }
+		public double rating { get; set; }
+	}
+
+	public class Uploader
+	{
+		public string _id { get; set; }
+		public string username { get; set; }
+	}
+
+	public class BeatSaverSong
+	{
+		public Metadata metadata { get; set; }
+		public Stats stats { get; set; }
+		public string description { get; set; }
+		public DateTime? deletedAt { get; set; }
+		public string _id { get; set; }
+		public string key { get; set; }
+		public string name { get; set; }
+		public Uploader uploader { get; set; }
+		public string hash { get; set; }
+		public DateTime uploaded { get; set; }
+		public string directDownload { get; set; }
+		public string downloadURL { get; set; }
+		public string coverURL { get; set; }
+	}
+}
